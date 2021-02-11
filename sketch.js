ec62cb8cc8075fe5f08811abc5deabe160d98e68
Purva Shukla <purvashukla828@gmail.com>
2021-02-11 15:56:11 +0530

    Initial code

---
+++ sketch.js
@@ -0,0 +1,102 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Render = Matter.Render;
+const Runner = Matter.Runner;
+const Composites = Matter.Composites;
+const MouseConstraint = Matter.MouseConstraint;
+const Mouse = Matter.Mouse;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+var Example = Example || {};
+var gameState = "onSling";
+var score = 0;
+//game sounds
+var birdSelectSound,birdFlySound,pigSnortSound;
+
+function preload(){
+    backgroundImg = loadImage("sprites/skyBackground.jpg");
+}
+
+function setup(){
+
+    engine = Engine.create();
+    world = engine.world;
+    // create renderer
+    var render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 600,
+            showAngleIndicator: true,
+            // wireframes: false
+            // background: 'sprites/skyBackground.jpg'
+        }
+    });
+
+    Render.run(render);
+
+    // create runner
+    var runner = Runner.create();
+    Runner.run(runner, engine);
+
+     // add bodies
+    var rows = 10,
+        yy = 600 - 25 - 40 * rows;
+    
+        var ball = Bodies.circle(100, 400, 50, { density: 0.04, frictionAir: 0.005});
+    
+    World.add(world, ball);
+    World.add(world, Constraint.create({
+        pointA: { x: 300, y: 100 },
+        bodyB: ball
+    }));
+
+    // add mouse control
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 0.01,
+                render: {
+                    visible: false
+                }
+            }
+        });
+
+    World.add(world, mouseConstraint);
+
+    // keep the mouse in sync with rendering
+    render.mouse = mouse;
+
+    // fit the render viewport to the scene
+    Render.lookAt(render, {
+        min: { x: 0, y: 0 },
+        max: { x: 800, y: 600 }
+    });
+
+    // context for MatterTools.Demo
+    return {
+        engine: engine,
+        runner: runner,
+        render: render,
+        canvas: render.canvas,
+        stop: function() {
+            Matter.Render.stop(render);
+            Matter.Runner.stop(runner);
+        }
+    };
+};
+
+Example.wreckingBall.for = '>=0.14.2';
+
+if (typeof module !== 'undefined') {
+    module.exports = Example.wreckingBall;
+}
+   
+
